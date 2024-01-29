ee4a43cb4c8f72dc4607805b5d61894cab81ecc3
Sharif Aly <sharif.aly_@outlook.com>
2024-01-29 20:55:55 +0100

    build: create jwt and store user id inside

--- index.js
+++ index.js
@@ -61,21 +61,10 @@ app.post("/login", (req, res) => {
         const user = result[0];
         const isPasswordValid = bcrypt.compareSync(password, user.password);
         if (isPasswordValid) {
-          const token = jwt.sign(
-            {
-              id: user.id,
-              email: user.email,
-            },
-            process.env.JWT_SECRET,
-            {
-              expiresIn: "1h",
-            }
-          );
-          res.send({
-            token: token,
-            id: user.id,
-            email: user.email,
+          const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+            expiresIn: "1h",
           });
+          res.json({ token });
         } else {
           res.status(401).send("Invalid Password");
         }
